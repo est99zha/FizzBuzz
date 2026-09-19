0a4c58dbd927f7f275fc18d967e56f0a8d24951f
est99zha <jingx.zhang@mail.utoronto.ca>
2026-09-18 21:07:11 -0400

    Modify Reduce.java

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -1,4 +1,25 @@
 package lab1;
 
 public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(Reduce(100));
+    }
+    static int Reduce(int n) {
+        return reduce(n);
+    }
+
+    static int reduce(int n) {
+        int result = 0;
+        while (n > 0) {
+            boolean iseven = n % 2 == 0;
+            boolean isodd = n % 2 == 1;
+            if (iseven){
+                n = n /2;
+            }else if (isodd){
+                n = n - 1;
+            }
+            result += 1;
+        }
+        return result;
+    }
 }
